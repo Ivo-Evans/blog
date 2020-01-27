1ca1247ae9cc3343d26b9638071a482b4f73cf22
Ivo Evans Storrie <ievans.storrie@gmail.com>
2020-01-27 12:48:09 +0000

    button message changes at end of transition

--- expander-2.js
+++ expander-2.js
@@ -3,8 +3,7 @@ let posts = Array.from(document.querySelectorAll('.post')).map((post) =>
       6, // starting lineheight - see CSS
       post.querySelector('.post-content'), // this strategy relies on having only one p element - actually quite implausible. Better, I think, to target the div with the class post-content
       post.querySelector('.expandContractButton')
-    ]
-  
+    ]  
 )
 
 posts.forEach(post => {
@@ -16,10 +15,8 @@ posts.forEach(post => {
 function expandOrContract(post) {
   if (post[2].innerText == 'Read more +') {
     expand(post);
-    post[2].innerText = "Read less -"; // could put a setTImeout to change this in expand
   } else if (post[2].innerText == "Read less -") {
     contract(post);
-    post[2].innerText = "Read more +";
   }
 }
 
@@ -30,7 +27,8 @@ function expand(post) {
     post[0]++;
     post[1].style.maxHeight = post[0] + "em";
     if (post[1].offsetHeight == recordedHeight) {
-      clearInterval(expansion)
+      clearInterval(expansion);
+      post[2].innerText = "Read less -"; // could put a setTImeout to change this in expand
     } else {
       recordedHeight = post[1].offsetHeight;
     }
@@ -41,6 +39,9 @@ function contract(post) {
   let contraction = setInterval(() => {
     post[0]--;
     post[1].style.maxHeight = post[0] + "em";
-    if (post[0] <= 6) {clearInterval(contraction)}
+    if (post[0] <= 6) {
+      clearInterval(contraction);
+      post[2].innerText = "Read more +";
+    }
   }, 20)
 }
