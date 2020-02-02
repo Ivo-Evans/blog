3be494aba8cd5882ac18923aeb30c590513a9fc0
Ivo Evans Storrie <ievans.storrie@gmail.com>
2020-02-02 17:30:54 +0000

    adjust compile_article function for clarity

--- resources/modules/compilable.rb
+++ resources/modules/compilable.rb
@@ -3,16 +3,21 @@ require './resources/modules/writable.rb'
 module Compilable
   @tab = "  "
 
-  def compile_article(title, tags, date, content)
-  
-    lines = [@tab * 2 + "<article class=\"post\">"]
-    lines.push(@tab * 3 + "<span class=\"post-title-bar\"><h2 id=\"#{title}\" class=\"post-title\">#{title}</h2><p class=\"meta-info\">#{date}<br>#{tags.join(", ")}</p></span>")
-    lines.push(@tab * 3 + "<div class=\"post-content\">")
+  def compile_article(title, tags, date, content)  
+    lines = [
+      @tab * 2 + "<article class=\"post\">",
+      @tab * 3 + "<span class=\"post-title-bar\"><h2 id=\"#{title}\" class=\"post-title\">#{title}</h2><p class=\"meta-info\">#{date}<br>#{tags.join(", ")}</p></span>",
+      @tab * 3 + "<div class=\"post-content\">"
+    ]
+
     content.each {|element| lines.push(@tab * 4 + element)}
-    lines.push(@tab * 4 + "<img class=\"fade\" src=\"./resources/bottom-fade.png\" alt=\"an image that adds a fade effect to the bottom layer\">")
-    lines.push(@tab * 3 + "</div>")
-    lines.push(@tab * 3 + "<h3 class=\"expandContractButton\">Read more +</h3>")
-    lines.push(@tab * 2 + "</article>")
+
+    lines + [
+      @tab * 4 + "<img class=\"fade\" src=\"./resources/bottom-fade.png\" alt=\"an image that adds a fade effect to the bottom layer\">",
+      @tab * 3 + "</div>",
+      @tab * 3 + "<h3 class=\"expandContractButton\">Read more +</h3>",
+      @tab * 2 + "</article>"
+    ]
   end
   
   def compile_archive(titles)
